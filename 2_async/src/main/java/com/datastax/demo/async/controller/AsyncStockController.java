cf8d6f5ac448fdc46c8e06d7fc7b3976fc35317c
Alexandre Dutra <alexdut@gmail.com>
2019-05-08 18:50:42 +0200

    Remove DeferredResult and simplify AsyncStockController

--- 2_async/src/main/java/com/datastax/demo/async/controller/AsyncStockController.java
+++ 2_async/src/main/java/com/datastax/demo/async/controller/AsyncStockController.java
@@ -15,12 +15,15 @@
  */
 package com.datastax.demo.async.controller;
 
+import static java.util.concurrent.CompletableFuture.completedStage;
+
 import com.datastax.demo.async.repository.AsyncStockRepository;
 import com.datastax.demo.common.controller.StockUriHelper;
 import com.datastax.demo.common.model.Stock;
 import com.datastax.oss.driver.api.core.DriverException;
 import java.net.URI;
 import java.time.Instant;
+import java.util.concurrent.CompletionStage;
 import java.util.stream.Stream;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -37,7 +40,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.async.DeferredResult;
 
 /** A REST controller that performs CRUD actions on {@link Stock} instances. */
 @RestController
@@ -74,21 +76,15 @@ public class AsyncStockController {
    * @return The created stocked value.
    */
   @PostMapping("")
-  public DeferredResult<ResponseEntity<Stock>> createStock(
+  public CompletionStage<ResponseEntity<Stock>> createStock(
       @RequestBody Stock stock, @NonNull HttpServletRequest request) {
-    var deferred = new DeferredResult<ResponseEntity<Stock>>();
-    stockRepository
+    return stockRepository
         .save(stock)
-        .whenComplete(
-            (created, error) -> {
-              if (error == null) {
-                URI location = uriHelper.buildDetailsUri(request, created);
-                deferred.setResult(ResponseEntity.created(location).body(created));
-              } else {
-                deferred.setErrorResult(error);
-              }
+        .thenApply(
+            created -> {
+              URI location = uriHelper.buildDetailsUri(request, created);
+              return ResponseEntity.created(location).body(created);
             });
-    return deferred;
   }
 
   /**
@@ -100,36 +96,18 @@ public class AsyncStockController {
    * @return The updated stock value.
    */
   @PutMapping("/{symbol}/{date}")
-  public DeferredResult<ResponseEntity<Stock>> updateStock(
+  public CompletionStage<ResponseEntity<Stock>> updateStock(
       @PathVariable("symbol") String symbol,
       @PathVariable("date") Instant date,
       @RequestBody Stock stock) {
-    var deferred = new DeferredResult<ResponseEntity<Stock>>();
-    stockRepository
+    return stockRepository
         .findById(symbol, date)
-        .whenComplete(
-            (maybeFound, error1) -> {
-              if (error1 == null) {
+        .thenCompose(
+            maybeFound ->
                 maybeFound
                     .map(found -> new Stock(found.getSymbol(), found.getDate(), stock.getValue()))
-                    .ifPresentOrElse(
-                        toUpdate ->
-                            stockRepository
-                                .save(toUpdate)
-                                .whenComplete(
-                                    (found, error2) -> {
-                                      if (error2 == null) {
-                                        deferred.setResult(ResponseEntity.ok(found));
-                                      } else {
-                                        deferred.setErrorResult(error2);
-                                      }
-                                    }),
-                        () -> deferred.setResult(ResponseEntity.notFound().build()));
-              } else {
-                deferred.setErrorResult(error1);
-              }
-            });
-    return deferred;
+                    .map(toUpdate -> stockRepository.save(toUpdate).thenApply(ResponseEntity::ok))
+                    .orElse(completedStage(ResponseEntity.notFound().build())));
   }
 
   /**
@@ -140,20 +118,9 @@ public class AsyncStockController {
    * @return An empty response.
    */
   @DeleteMapping("/{symbol}/{date}")
-  public DeferredResult<ResponseEntity<Void>> deleteStock(
+  public CompletionStage<ResponseEntity<Void>> deleteStock(
       @PathVariable("symbol") String symbol, @PathVariable("date") Instant date) {
-    var deferred = new DeferredResult<ResponseEntity<Void>>();
-    stockRepository
-        .deleteById(symbol, date)
-        .whenComplete(
-            (res, error) -> {
-              if (error == null) {
-                deferred.setResult(ResponseEntity.ok().build());
-              } else {
-                deferred.setErrorResult(error);
-              }
-            });
-    return deferred;
+    return stockRepository.deleteById(symbol, date).thenApply(ok -> ResponseEntity.ok().build());
   }
 
   /**
@@ -164,22 +131,12 @@ public class AsyncStockController {
    * @return The found stock value, or empty if no stock value was found.
    */
   @GetMapping("/{symbol}/{date}")
-  public DeferredResult<ResponseEntity<Stock>> findStock(
+  public CompletionStage<ResponseEntity<Stock>> findStock(
       @PathVariable("symbol") String symbol, @PathVariable("date") Instant date) {
-    var deferred = new DeferredResult<ResponseEntity<Stock>>();
-    stockRepository
+    return stockRepository
         .findById(symbol, date)
-        .whenComplete(
-            (stock, error) -> {
-              if (error == null) {
-                var response =
-                    stock.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
-                deferred.setResult(response);
-              } else {
-                deferred.setErrorResult(error);
-              }
-            });
-    return deferred;
+        .thenApply(
+            stock -> stock.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build()));
   }
 
   /**
@@ -194,24 +151,15 @@ public class AsyncStockController {
    * @return The available stocks for the given symbol and date range.
    */
   @GetMapping("/{symbol}")
-  public DeferredResult<ResponseEntity<Stream<Stock>>> listStocks(
+  public CompletionStage<ResponseEntity<Stream<Stock>>> listStocks(
       @PathVariable(name = "symbol") @NonNull String symbol,
       @RequestParam(name = "start") @NonNull Instant startInclusive,
       @RequestParam(name = "end") @NonNull Instant endExclusive,
       @RequestParam(name = "offset") int offset,
       @RequestParam(name = "limit") int limit,
       @NonNull HttpServletRequest request) {
-    var deferred = new DeferredResult<ResponseEntity<Stream<Stock>>>();
-    stockRepository
+    return stockRepository
         .findAllBySymbol(symbol, startInclusive, endExclusive, offset, limit)
-        .whenComplete(
-            (results, error) -> {
-              if (error == null) {
-                deferred.setResult(ResponseEntity.ok(results));
-              } else {
-                deferred.setErrorResult(error);
-              }
-            });
-    return deferred;
+        .thenApply(ResponseEntity::ok);
   }
 }

6d29ffcdd9f2319ed93c662a22f44bd966be91d5
Alexandre Dutra <alexdut@gmail.com>
2019-05-08 22:19:22 +0200

    Reorder methods

--- 3_reactive/src/main/java/com/datastax/demo/reactive/controller/ReactiveStockController.java
+++ 3_reactive/src/main/java/com/datastax/demo/reactive/controller/ReactiveStockController.java
@@ -67,42 +67,6 @@ public class ReactiveStockController {
   }
 
   /**
-   * Lists the available stocks for the given symbol and date range (GET method).
-   *
-   * @param symbol The symbol to list stocks for.
-   * @param startInclusive The start of the date range (inclusive).
-   * @param endExclusive The end of the date range (exclusive).
-   * @param offset The zero-based index of the first result to return.
-   * @param limit The maximum number of results to return.
-   * @return The available stocks for the given symbol and date range.
-   */
-  @GetMapping("/{symbol}")
-  public Flux<Stock> listStocks(
-      @PathVariable(name = "symbol") @NonNull String symbol,
-      @RequestParam(name = "start") @NonNull Instant startInclusive,
-      @RequestParam(name = "end") @NonNull Instant endExclusive,
-      @RequestParam(name = "offset") int offset,
-      @RequestParam(name = "limit") int limit) {
-    return stockRepository.findAllBySymbol(symbol, startInclusive, endExclusive, offset, limit);
-  }
-
-  /**
-   * Retrieves the stock value for the given symbol and date (GET method).
-   *
-   * @param symbol The stock symbol to find.
-   * @param date The stock date to find.
-   * @return The found stock value, or empty if no stock value was found.
-   */
-  @GetMapping("/{symbol}/{date}")
-  public Mono<ResponseEntity<Stock>> findStock(
-      @PathVariable("symbol") String symbol, @PathVariable("date") Instant date) {
-    return stockRepository
-        .findById(symbol, date)
-        .map(ResponseEntity::ok)
-        .defaultIfEmpty(ResponseEntity.notFound().build());
-  }
-
-  /**
    * Creates a new stock value (POST method).
    *
    * @param stock The stock value to create.
@@ -151,5 +115,41 @@ public class ReactiveStockController {
   public Mono<ResponseEntity<Void>> deleteStock(
       @PathVariable("symbol") String symbol, @PathVariable("date") Instant date) {
     return stockRepository.deleteById(symbol, date).then(Mono.just(ResponseEntity.ok().build()));
+  }
+
+  /**
+   * Retrieves the stock value for the given symbol and date (GET method).
+   *
+   * @param symbol The stock symbol to find.
+   * @param date The stock date to find.
+   * @return The found stock value, or empty if no stock value was found.
+   */
+  @GetMapping("/{symbol}/{date}")
+  public Mono<ResponseEntity<Stock>> findStock(
+      @PathVariable("symbol") String symbol, @PathVariable("date") Instant date) {
+    return stockRepository
+        .findById(symbol, date)
+        .map(ResponseEntity::ok)
+        .defaultIfEmpty(ResponseEntity.notFound().build());
+  }
+
+  /**
+   * Lists the available stocks for the given symbol and date range (GET method).
+   *
+   * @param symbol The symbol to list stocks for.
+   * @param startInclusive The start of the date range (inclusive).
+   * @param endExclusive The end of the date range (exclusive).
+   * @param offset The zero-based index of the first result to return.
+   * @param limit The maximum number of results to return.
+   * @return The available stocks for the given symbol and date range.
+   */
+  @GetMapping("/{symbol}")
+  public Flux<Stock> listStocks(
+      @PathVariable(name = "symbol") @NonNull String symbol,
+      @RequestParam(name = "start") @NonNull Instant startInclusive,
+      @RequestParam(name = "end") @NonNull Instant endExclusive,
+      @RequestParam(name = "offset") int offset,
+      @RequestParam(name = "limit") int limit) {
+    return stockRepository.findAllBySymbol(symbol, startInclusive, endExclusive, offset, limit);
   }
 }
